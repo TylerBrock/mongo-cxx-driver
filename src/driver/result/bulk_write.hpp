b027f6873203a78b2bf869fcc441daa80378ecea
Tyler Brock <tyler.brock@gmail.com>
2014-09-03 16:33:59 -0400

    add new result headers

---
+++ src/driver/result/bulk_write.hpp
@@ -0,0 +1,45 @@
+// Copyright 2014 MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "driver/config/prelude.hpp"
+
+#include <cstdint>
+#include <map>
+
+#include "bson/types.hpp"
+
+namespace mongo {
+namespace driver {
+namespace result {
+
+struct LIBMONGOCXX_EXPORT bulk_write {
+    bool is_acknowledged;
+
+    std::int64_t inserted_count;
+    std::int64_t matched_count;
+    std::int64_t modified_count;
+    std::int64_t removed_count;
+    std::int64_t upserted_count;
+
+    std::map<std::size_t, bson::element> inserted_ids;
+    std::map<std::size_t, bson::element> upserted_ids;
+}
+
+}  // namespace result
+}  // namespace driver
+}  // namespace mongo
+
+#include "driver/config/postlude.hpp"
