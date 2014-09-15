818bf723fc9e3d7b1d1cfe7ee4b74f52a3f74ebe
Tyler Brock <tyler.brock@gmail.com>
2014-09-15 15:26:27 -0400

    handle failures better in scons for examples

--- site_scons/site_tools/integration_test.py
+++ site_scons/site_tools/integration_test.py
@@ -9,6 +9,7 @@ def build_integration_test(env, target, source, **kwargs):
     buildAlias = env.Alias('build-' + target, result)
     env.Alias('build-integration', buildAlias)
     env['ENV']['GTEST_FILTER'] = env.get("gtest_filter", "*")
+    env['ENV']['GTEST_BREAK_ON_FAILURE'] = 1;
     runAlias = env.Alias('run-' + target, [result],
         "%s localhost:8889" % result[0].abspath)
     env.AlwaysBuild(runAlias)
